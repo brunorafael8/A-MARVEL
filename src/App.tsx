0e29ee7114ee1feb9aadeb39ad74258f06c0358a
Bruno Rafael <bruninho_lito@hotmail.com>
2020-11-29 20:32:31 -0300

    refactor: App src

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+declare const global: { HermesInternal: null | {} };
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView></SafeAreaView>
+    </>
+  );
+};
+
+export default App;
